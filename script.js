a81afc421b251c8263b3a08bd30dab53b31106c7
rayaneschneider <105427657+rayaneschneider@users.noreply.github.com>
2024-06-07 08:47:42 -0300

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+    const caixaPrincipal= document.querySelector(".caixa-principal");
+    const caixaPerguntas= document.querySelector("caixa-pergunta");
+    const caixaAlternativas= document.querySelector(".caixa-alternativa");
+    const caixaResultado= document.querySelector(".caixa-resultado");
+    const textoResultado= document.querySelector(".caixa-resultado");
+    const perguntas = [
+        { //primeiro objeto da lista de perguntas
+            enunciado:"pergunta1",
+              alternativas: ["alternativas","alternativas2"]
+            },
+            {//segundo objetivo da lista de perguntas
+                enunciado: "pergunta2",
+               alternativas:["alternativa1"],["alternativa2"]
+            }
+    ]
+    
